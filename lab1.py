9c26f1fa9e732a91115f44a82d7047027ab107a8
Vivek Malhotra <malhotra@ualberta.ca>
2023-01-11 18:07:51 -0700

    file download itself

--- lab1.py
+++ lab1.py
@@ -3,3 +3,6 @@ print(requests.__version__)
 
 response = requests.get("http://www.google.com/")
 print(response)
+
+file_download = requests.get("https://raw.githubusercontent.com/vivek-rgb/cmput404-labs/master/lab1.py")
+print(file_download)
