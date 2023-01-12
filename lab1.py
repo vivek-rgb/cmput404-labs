348e31979286f88ce600cd31e76966d2cde5c1bf
Vivek Malhotra <malhotra@ualberta.ca>
2023-01-11 18:09:35 -0700

    print itself

--- lab1.py
+++ lab1.py
@@ -2,7 +2,9 @@ import requests
 print(requests.__version__)
 
 response = requests.get("http://www.google.com/")
+
+print("PRINTING RESPONSE: ")
 print(response)
 
 file_download = requests.get("https://raw.githubusercontent.com/vivek-rgb/cmput404-labs/master/lab1.py")
-print(file_download)
+print(file_download.text)
